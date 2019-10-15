c15fec8233074a1b07ae94f542443e7ed7d6bce4
Mateus Gurgel <mgurgel5855@conestogac.on.ca>
2019-10-15 19:34:09 -0400

    :sparkles: Add logging and SOAP Fault exception generation

--- soapserver/services/ip/service.py
+++ soapserver/services/ip/service.py
@@ -1,3 +1,6 @@
+import logging
+logger = logging.getLogger(__name__)
+
 from spyne.decorator import srpc
 from spyne.service import ServiceBase
 
@@ -6,6 +9,7 @@ from spyne.model.primitive import UnsignedInteger
 from spyne.model.primitive import String
 from spyne.model.primitive import Float
 from spyne.model.complex import ComplexModel
+from spyne.model.fault import Fault
 
 # Functions
 from . import ip_resolver as ipr
@@ -19,10 +23,22 @@ class IpInfo(ComplexModel):
     Latitude = Float
     Longitude = Float
 
+class InvalidIPAddress(Fault):
+    def __init__(self, faultstring):
+        super(InvalidIPAddress, self) \
+            .__init__(faultcode="Client.InvalidIPAddress", faultstring=faultstring)
+
+
 class ResolveIP(ServiceBase):
     @srpc(String, _returns=IpInfo)
     def GetInfo(ipAddr):
-        ipInfo = ipr.resolveIP(ipAddr)
+        try:
+            ipInfo = ipr.resolveIP(ipAddr)
+            logger.debug("Successful request to 'GetInfo'")
+        except Exception as e:
+            logger.info("Invalid request to 'GetInfo' – Reason: %s" % e.args[0])
+            raise InvalidIPAddress(e.args[0])
+
         return IpInfo (
             City=ipInfo.city,
             StateProvince=ipInfo.stateProvince,
